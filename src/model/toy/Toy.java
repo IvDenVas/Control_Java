3f1fed00297f6d39249761cb9fdcaca920760dc6
Denis <ivdenvas@gmail.com>
2023-06-14 10:19:06 +0300

    start

---
+++ src/model/toy/Toy.java
@@ -0,0 +1,46 @@
+package model.toy;
+public class Toy {
+    private int id;
+    private String name;
+    private int count;
+    private int weight;
+
+    public Toy(int id, String name, int count, int weight) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
